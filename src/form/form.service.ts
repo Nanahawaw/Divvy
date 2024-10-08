818dbe7c727eb0507883dd733286326c44b7a1e5
Nanahawaw <folarinyetunde@gmail.com>
2024-10-08 19:15:32 +0100

    remove specs

--- src/form/form.service.ts
+++ src/form/form.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+import { FormSubmission } from 'src/form/submission.entity';
+import { FormSubmissionDto } from 'src/form/dto/form-submission.dto';
 
 @Injectable()
-export class FormService {}
+export class FormService {
+  constructor(
+    @InjectRepository(FormSubmission)
+    private formSubmissionRepository: Repository<FormSubmission>,
+  ) {}
+  async submitForm(
+    formSubmissionDto: FormSubmissionDto,
+  ): Promise<FormSubmission> {
+    const formSubmission =
+      this.formSubmissionRepository.create(formSubmissionDto);
+    return await this.formSubmissionRepository.save(formSubmission);
+  }
+}
