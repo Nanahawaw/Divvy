5f05b640873c2ee60b0d5871dd604c532b382510
Nanahawaw <folarinyetunde@gmail.com>
2024-10-09 21:04:12 +0100

    fixed cors issue

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,9 @@ import { AppModule } from './app.module';
 import * as express from 'express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
+  });
   app.use('/uploads', express.static('uploads'));
   // Listen on all interfaces
   await app.listen(process.env.PORT || 3000, '0.0.0.0');
